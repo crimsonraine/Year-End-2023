5216fc8c14248522aaf138cc55743e8872e5c78e
J-Tell <fairyq007@hotmail.com>
2023-06-12 10:08:01 -0400

    dialogue box works!!!!!! (thanks to kimby)

--- practicescene.js
+++ practicescene.js
@@ -1,4 +1,3 @@
-// The game config that is used by Phaser
 var config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
@@ -9,8 +8,8 @@ var config = {
     create: create
   }
 };
-// Create a new Phaser Game object
 var game = new Phaser.Game(config);
+const list = ["Greetings, traveler?", "What brings you here?", "Are you lost?", "hello!"]
 function preload () {
   this.load.plugin('DialogModalPlugin', './dialog_plugin.js');
 }
@@ -18,9 +17,11 @@ function create () {
   this.sys.install('DialogModalPlugin');
 console.log(this.sys.dialogModal);
 this.sys.dialogModal.init();
-this.sys.dialogModal.setText('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', true);
-this.time.addEvent({
-  delay : 3000
-})
-this.sys.dialogModal.setText('help me', true);
+let sword_button = this.add.text(670, 530, 'ARROW', {fontFamily: 'Press-Start-2P', fill : '#5f3b39'}).setScale(1.4);
+sword_button.setInteractive();
+let i = -1;
+sword_button.on('pointerdown', () => {
+  i += 1;
+  this.sys.dialogModal.setText(list[i], true)
+});
 }
